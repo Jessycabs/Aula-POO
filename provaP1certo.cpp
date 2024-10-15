d3ca0768d4c24a5b12fabcc8f1bebad9d34e876d
Jessycabs <140827501+Jessycabs@users.noreply.github.com>
2024-10-15 20:09:53 -0300

    Add files via upload

---
+++ provaP1certo.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace sdt;
+
+int a, b, c, d, i, j;
+
+int main(){
+
+//gl = 0;
+//gc = 1;
+i = 1;
+
+cout << ">> inicio prova P1";
+cout << "informe 4 valores: " << endl;
+cout << "valor 1: "
+cin >> a;
+cout << "valor 2: "
+cin >> b;
+cout << "valor 3: "
+cin >> c;
+cout << "valor 4: "
+cin >> d;
+
+
+cout << "total de linhas: 4";
+cout << "total de colunas: 5";
+
+
+
+do
+    j = 0;
+    cout << endl;
+    cout << i << "|";
+    if (a != 0){
+    cout << " * ";
+    }
+    j = j + 1;
+while (j = a)
+    ++i
+
+do
+    j = 0;
+    cout << endl;
+    cout << i << "|";
+    if (b != 0){
+    cout << " * ";
+    }
+    j = j + 1;
+while (j = b)
+    ++i
+
+do
+    j = 0;
+    cout << endl;
+    cout << i << "|";
+    if (c != 0){
+    cout << " * ";
+    }
+    j = j + 1;
+while (j = c)
+    ++i
+
+do
+    j = 0;
+    cout << endl;
+    cout << i << "|";
+    if (d != 0){
+    cout << " * ";
+    }
+    j = j + 1;
+while (j = d)
+    ++i
+
+    cout << ">> Fim: prova P1"
+return 0;
+}
